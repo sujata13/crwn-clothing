9db33053880b59066cedebbf09e3baa0c2013e13
Sujata Mishra <mishrasujata1628@gmail.com>
2021-09-21 22:28:51 +0530

    added size props

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl }) =>(
+const MenuItem = ({ title, imageUrl, size }) =>(
     <div className={`${size} menu-item`}
     >
         <div className='background-image' style={{
